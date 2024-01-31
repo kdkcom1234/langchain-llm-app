833915a5eb358b1fab5a6981930d9caa2a5226a9
Daekeun Ko <56523408+kdkcom1234@users.noreply.github.com>
2024-01-31 13:12:17 +0900

    Update langchain_helper.py

--- youtube-assistant/langchain_helper.py
+++ youtube-assistant/langchain_helper.py
@@ -24,6 +24,8 @@ def create_vertor_db_from_youtube_url(video_url: str) -> FAISS:
   text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
   docs = text_splitter.split_documents(transcript)
 
+  # 파시스는 먼저 데이터를 양자화하여 인덱스를 생성하고, 이후 이 인덱스를 사용하여 유사성 검색을 수행합니다.
+  # https://dajeblog.co.kr/16-faiss%EC%97%90-%EB%8C%80%ED%95%9C-%EB%AA%A8%EB%93%A0-%EA%B2%83/
   db = FAISS.from_documents(docs, embeddings)
   return db
 
